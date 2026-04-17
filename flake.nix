5f97f1693b2ee4ec8c6bb2bd87485d4f80e88a11
nicolasdumitru <nicolas@ndumitru.com>
2026-04-17 13:52:29 +0300

    flake: pwndbg & ghidra

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pwndbg.url = "github:pwndbg/pwndbg";
   };
   outputs =
     {
       nixpkgs,
+      pwndbg,
       ...
     }:
     let
@@ -18,6 +20,8 @@
         packages = with pkgs; [
           # WARNING: Do NOT add gcc, clang, gligc, libcxx, etc.
           # They will break the environment, leading to compilation or LSP errors.
+          
+          # Note: Objdump is packaged with the Clang toolchain
           clang-tools
           meson
           ninja
@@ -27,9 +31,10 @@
           gnumake
 
           gdb
+          pwndbg.packages."${system}".default
           valgrind
 
-          # ghidra
+          ghidra
 
           file
 
