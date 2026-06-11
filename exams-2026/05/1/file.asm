99620451f0b872fbc0bc1633651f725af9dc7265
nicolasdumitru <nicolas@ndumitru.com>
2026-06-12 00:51:14 +0300

    solve 05.01.a

--- exams-2026/05/1/file.asm
+++ exams-2026/05/1/file.asm
@@ -4,24 +4,52 @@ section .data
     plaintext   db  "Hello, Worldz!", 0
     SHIFT       equ 3
  
+    vowels  db "aeiou", 0
+ 
     fmt_enc     db  "Encoded : %s", 10, 0
     fmt_dec     db  "Decoded : %s", 10, 0
+    
+    fmt_vowels db "Vowels : %ld", 0
  
 section .bss
     encoded     resb 64
     decoded     resb 64
  
 
-section .text
 extern printf
+extern strchr
 global main
 
+section .text
 main:
     push rbp
     mov rbp, rsp
 
     ; TODO a: Count the number of vowels in plaintext.
 
+    xor rcx, rcx ; rcx = 0
+    xor r12, r12 ; rbx = 0
+.loop1:
+    mov al, [plaintext + rcx]
+    cmp al, 0
+    je .loop1_end
+    
+    mov rdi, vowels
+    movzx rsi, al
+    call strchr
+    cmp rax, 0
+    je .iteration_end
+    inc r12
+.iteration_end:
+    inc rcx
+    jmp .loop1
+.loop1_end:
+
+    mov rdi, fmt_vowels
+    mov rsi, r12
+    xor rax, rax
+    
+    call printf
 
     ; TODO b: Shift all letters inside the plaintext string with SHIFT positions
     ; make sure to loop from z to a, e.g. y will become b (y -> z -> a -> b).
@@ -39,3 +67,5 @@ main:
     xor rax, rax
     leave
     ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
